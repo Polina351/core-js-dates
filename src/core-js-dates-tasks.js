12a64ba24b4d2f29a0999744d32f27f839182b28
Polina Deniskina <polinaoioi67@gmail.com>
2024-03-25 19:28:52 +0000

    feat: solve two tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -284,8 +284,30 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const start = new Date(period.start.split('-').reverse().join('-'));
+  const end = new Date(period.end.split('-').reverse().join('-'));
+
+  let isWorkDay = true;
+  let currentDay = start;
+  const res = [];
+
+  while (currentDay <= end) {
+    if (isWorkDay) {
+      for (let i = 0; i < countWorkDays && currentDay <= end; i += 1) {
+        res.push(currentDay.toJSON());
+        currentDay = new Date(currentDay.setDate(currentDay.getDate() + 1));
+      }
+      isWorkDay = false;
+    }
+    if (!isWorkDay) {
+      currentDay = new Date(
+        currentDay.setDate(currentDay.getDate() + countOffDays)
+      );
+      isWorkDay = true;
+    }
+  }
+  return res.map((item) => item.slice(0, 10).split('-').reverse().join('-'));
 }
 
 /**
@@ -300,8 +322,8 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  return new Date(date.getUTCFullYear(), 2, 0).getDate() === 29;
 }
 
 module.exports = {
